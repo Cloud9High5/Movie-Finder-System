748da4a36f6bea3cb58ba2efe339a89b146ae079
Qizhou XIE <lennoxxqz@gmail.com>
2022-06-21 16:18:17 +0800

    UI of sign up page

--- front-end/src/components/navBar.jsx
+++ front-end/src/components/navBar.jsx
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
-import { hover } from "@testing-library/user-event/dist/hover";
 
 const style = makeStyles({
   box: {
@@ -26,9 +25,13 @@ const NavBar = () => {
   const [token, setToken] = React.useState('');
   const path = useNavigate();
   const classes = style();
-  
+
   function toLogin () {
     path('/login');
+  }
+
+  function toSignUp () {
+    path('/signup');
   }
 
   function toSignUp () {
@@ -39,13 +42,16 @@ const NavBar = () => {
     <Box className={classes.box}>
       <AppBar position="static">
         <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography variant="h6" component="div" classes={classes.logo} onClick={toLogin} sx={{ transition: "color .3s ease-in-out, box-shadow .3s ease-in-out", ":hover": { boxShadow: "inset 100px 0 0 0 #54b3d6", color: "whitesmoke", textDecoration: "",} }}>
+          <Typography variant="h6" component="div" classes={classes.logo} onClick={toLogin} sx={{
+            transition: "color .3s ease-in-out, box-shadow .3s ease-in-out",
+            ":hover": { boxShadow: "inset 100px 0 0 0 #54b3d6", color: "whitesmoke", textDecoration: "", }
+          }}>
             DOUBI
           </Typography>
           <span>
             <Button color="inherit" onClick={toLogin} sx={{ textTransform: 'none' }}>Login</Button>
             <span> | </span>
-            <Button color="inherit" onClick={toLogin} sx={{ textTransform: 'none' }}>Sign Up</Button>
+            <Button color="inherit" onClick={toSignUp} sx={{ textTransform: 'none' }}>Sign Up</Button>
           </span>
         </Toolbar>
       </AppBar>
