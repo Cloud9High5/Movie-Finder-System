0cb18953baa4bc4c6e6b05b089455dd25e8fc1fb
Otto <zzy801997@163.com>
2022-06-23 02:24:50 +1000

    init reviews api

--- back-end/review.py
+++ back-end/review.py
@@ -1,7 +1,12 @@
 import sqlite3
 import os
+import pandas as pd
 
 filename = 'reviews.db'
 path = os.path.join(os.path.dirname(__file__), 'db', filename)
 
-print(path)
+# def insert_review(review):
+#     conn = sqlite3.connect(path)
+#     c = conn.cursor()
+#     c.execute("""INSERT INTO reviews (
+#         review, rating, uid, title, movie_id) VALUES (
