1a77dbb7f807e1b529eed806092a7d81770a5df9
Otto <zzy801997@163.com>
2022-06-23 17:37:16 +1000

    review db created

--- back-end/review.py
+++ back-end/review.py
@@ -5,6 +5,28 @@ import pandas as pd
 filename = 'reviews.db'
 path = os.path.join(os.path.dirname(__file__), 'db', filename)
 
+
+def init_review_db():
+    # create a new database for user if it doesn't exist
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+
+    # create a table for user if it doesn't exist
+    c.execute("""CREATE TABLE IF NOT EXISTS reviews (
+        review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        review TEXT,
+        rating INTEGER NOT NULL,
+        uid INTEGER NOT NULL,
+        movie_id INTEGER NOT NULL,
+        like INTEGER DEFAULT 0,
+        dislike INTEGER DEFAULT 0)""")
+    
+    conn.commit()
+    conn.close()
+
+
+
+
 # def insert_review(review):
 #     conn = sqlite3.connect(path)
 #     c = conn.cursor()
