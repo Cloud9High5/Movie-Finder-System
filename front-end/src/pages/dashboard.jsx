6b3c74c683813135b235b7565f14e4036b162feb
Qizhou XIE <lennoxxqz@gmail.com>
2022-06-24 01:13:04 +0800

    generate random movies' ID and wait for further process

--- front-end/src/pages/dashboard.jsx
+++ front-end/src/pages/dashboard.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Header from "../components/header";
+import DashboardMovieCard from "../components/dashboardMovieCard";
 
 
 function Dashboard () {
-  return (
-    <Container>
-      <Header/>
-      <div style={{ fontSize: 40 }}>Dashboard Page</div>
-    </Container>
-  )
+    const [movieInfo, setMovieInfo] = React.useState([]);
+
+    // generate a list of random movie id
+    React.useEffect(() => {
+        const tempID = [];
+        while (tempID.length < 3) {
+            const num = Math.floor(Math.random() * 100);
+            if (tempID.indexOf(num) === -1) {
+                tempID.push(num);
+            }
+        }
+
+        const tempInfo = [...movieInfo];
+        tempID.map(async (id) => {
+            const response = await fetch('http://127.0.0.1:5000/films?id=' + id);
+            const data = await response.json();
+            // console.log(tempInfo);
+            console.log(data);
+            tempInfo.push(data);
+            setMovieInfo(tempInfo);
+        })
+    }, [])
+
+    console.log(movieInfo)
+    return (
+        <Container>
+            <Header/>
+            <div style={{ fontSize: 40 }}>Dashboard Page</div>
+            {}
+            {movieInfo.map((movie, idx) => {
+                return (
+                    <DashboardMovieCard key={idx}/>
+                    // <h1 key={idx}> ${movie.title} </h1>
+                )
+            })}
+        </Container>
+    )
 }
 
 export default Dashboard;
