64f59e2882c641733d45e577672e7f6cd5e3da04
Qizhou XIE <lennoxxqz@gmail.com>
2022-06-25 23:15:34 +0800

    updates on dashboard

--- front-end/src/pages/dashboard.jsx
+++ front-end/src/pages/dashboard.jsx
@@ -3,7 +3,7 @@ import Container from '@material-ui/core/Container';
 import Header from "../components/header";
 import DashboardMovieCard from "../components/dashboardMovieCard";
 import { makeStyles } from '@material-ui/styles';
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from "@mui/material";
+import { Chip, Divider, FormControl, FormHelperText, MenuItem, Select } from "@mui/material";
 
 const styles = makeStyles({
     cards: {
@@ -35,7 +35,7 @@ function Dashboard () {
             const response = await fetch('http://127.0.0.1:5000/films?id=' + id);
             const data = await response.json();
             // console.log(tempInfo);
-            console.log(data);
+            // console.log(data);
             tempInfo.push(data);
             setMovieInfo(tempInfo);
         })
@@ -48,26 +48,29 @@ function Dashboard () {
     return (
         <Container>
             <Header/>
-            <div style={{ fontSize: 40 }}>Dashboard Page</div>
+            {/*<div style={{ fontSize: 40 }}>Dashboard Page</div>*/}
 
-            <FormControl sx={{ m: 1, minWidth: 120 }} size={'small'}>
-                <Select
-                    value={displayMode}
-                    onChange={changeDisplayMode}
-                >
-                    <MenuItem value={'random'}>Random Movies</MenuItem>
-                    <MenuItem value={'highest'}>Highest Rating</MenuItem>
-                    <MenuItem value={'latest'}>Latest Released</MenuItem>
-                </Select>
-                <FormHelperText>Click to change the display mode.</FormHelperText>
-            </FormControl>
-
+            <Divider variant="middle" textAlign={'left'}>
+                {/*<Chip label="Movies"/>*/}
+                <FormControl sx={{ m: 1, minWidth: 120 }} size={'small'}>
+                    <Select
+                        value={displayMode}
+                        onChange={changeDisplayMode}
+                    >
+                        <MenuItem value={'random'}>Random Movies</MenuItem>
+                        <MenuItem value={'highest'}>Highest Rating</MenuItem>
+                        <MenuItem value={'latest'}>Latest Released</MenuItem>
+                    </Select>
+                    <FormHelperText>Click to change the display mode.</FormHelperText>
+                </FormControl>
+            </Divider>
             <div className={classes.cards}>
-            {movieInfo.map((movie, idx) => {
-                return (
-                    <DashboardMovieCard key={idx} title={movie.title} poster={movie.poster} rating={movie.rating}/>
-                )
-            })}
+                {movieInfo.map((movie, idx) => {
+                    return (
+                        <DashboardMovieCard key={idx} title={movie.title} poster={movie.poster}
+                                            rating={movie.rating}/>
+                    )
+                })}
             </div>
         </Container>
     )
