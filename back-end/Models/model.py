19c813c9f0856a91bb5bb16cebf51c63a8b95b53
Otto <zzy801997@163.com>
2022-07-12 01:25:13 +1000

    change db interaction package from sqlite3 to sqlalchemy and re-organize database

---
+++ back-end/Models/model.py
@@ -0,0 +1,44 @@
+import datetime 
+from app import db
+from .helper import u_id_generator, f_id_generator, r_id_generator
+
+class User(db.Model):
+    __tablename__ = 'user'
+    u_id = db.Column(db.String(128), primary_key=True, nullable=False, unique=True, default=u_id_generator)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    photo_url = db.Column(db.String(80), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_blocked = db.Column(db.Boolean, nullable=False, default=False)
+    created_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    
+
+class Film(db.Model):
+    f_id = db.Column(db.String(128), primary_key=True, nullable=False, unique=True, default=f_id_generator)
+    title = db.Column(db.String(80), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    run_time = db.Column(db.String(16), nullable=False)
+    rating_imdb = db.Column(db.Float, nullable=False)
+    overview = db.Column(db.String(500), nullable=False)
+    director = db.Column(db.String(80), nullable=False)
+    url_poster = db.Column(db.String(500), nullable=False)
+    created_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+
+class Review(db.Model):
+    r_id = db.Column(db.String(128), primary_key=True, nullable=False, unique=True, default=r_id_generator)
+    u_id = db.Column(db.String(128), db.ForeignKey('user.u_id'), nullable=False)
+    f_id = db.Column(db.String(128), db.ForeignKey('film.f_id'), nullable=False)
+    content = db.Column(db.String(500), nullable=True)
+    rating = db.Column(db.Integer, nullable=False)
+    created_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+
+
+class Review_Like(db.Model):
+    r_id = db.Column(db.String(128), db.ForeignKey('review.r_id'), primary_key=True, nullable=False)
+    u_id = db.Column(db.String(128), db.ForeignKey('user.u_id'), primary_key=True, nullable=False)
+    is_liked = db.Column(db.Boolean, nullable=False, default=True)
+    created_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin  # CORS support, DO NOT DELETE
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)  # CORS support, DO NOT DELETE
 
 from APIs import api_bp
 app.register_blueprint(api_bp)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database/doubi_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
