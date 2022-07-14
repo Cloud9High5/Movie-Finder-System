ff25ee2daa05c8748428ef8c87d51e45da95966e
Otto <zzy801997@163.com>
2022-07-15 02:25:41 +1000

    update auth route func with new db

--- back-end/APIs/auth.py
+++ back-end/APIs/auth.py
@@ -46,7 +46,7 @@ user_profile_model = api.model('user', {
 })
 
 modify_list_argument = reqparse.RequestParser()
-modify_list_argument.add_argument('u_id', type=str, required=True)
+modify_list_argument.add_argument('target_id', type=str, required=True)
 
 resetpwd_arguments = reqparse.RequestParser()
 resetpwd_arguments.add_argument('email', type=str, required=True, help='Email is required')
@@ -68,6 +68,9 @@ resetpwd_model = api.model('resetpwd', {
 @api.route('/auth/signup', methods=['GET', 'POST'])
 class signup(Resource):
 
+    ########################################
+    #        send verification email       #
+    ########################################
     @api.doc(
         description="Send a verification email to check if the email is valid",
         responses={
@@ -101,6 +104,9 @@ class signup(Resource):
             mail.send(msg)
             return {'message': 'Email sent'}, 200
 
+    ########################################
+    #             Sign up user             #
+    ########################################
     @api.doc(
         description="Sign up a new user",
         responses = {
@@ -125,6 +131,9 @@ class signup(Resource):
 @api.route('/auth/resetpwd', methods=['GET', 'POST'])
 class resetpwd(Resource):
 
+    ########################################
+    #       send reset password email      #
+    ########################################
     @api.doc(
         description="Get user email and send a reset password verification email",
         responses = {
@@ -159,7 +168,10 @@ class resetpwd(Resource):
             return {'message': 'Email sent'}, 200
         else:
             return {'message': 'User not found'}, 404
-    
+
+    ########################################
+    #            reset password            #
+    ########################################    
     @api.doc(
         description="Reset user password",
         responses = {
@@ -188,6 +200,9 @@ class resetpwd(Resource):
 @api.route('/auth/login', methods=['GET', 'POST'])
 class login(Resource):
 
+    ########################################
+    #                 Login                #
+    ########################################
     @api.doc(
         description = 'Login to the system',
         responses = {
@@ -226,6 +241,9 @@ class login(Resource):
 @api.route('/auth/user/<string:u_id>', methods=['GET', 'PUT'])
 class user_info(Resource):
 
+    ########################################
+    #             Get user info            #
+    ########################################
     @api.doc(
         description = 'Get User Info by u_id',
         params={'u_id': 'User ID'},
@@ -257,6 +275,10 @@ class user_info(Resource):
             403: 'Fail, wrong password'
         }
     )
+
+    ########################################
+    #            Update User Info          #
+    ########################################
     @api.expect(user_profile_model, validate=True)
     def put(self, u_id):
         payload = json.loads(str(request.data, 'utf-8'))
@@ -288,6 +310,9 @@ class user_info(Resource):
 @api.route('/auth/user/<string:u_id>/following_list', methods=['GET','POST'])
 class following_list(Resource):
 
+    ########################################
+    #            Get following list        #
+    ########################################
     @api.doc(
         description = 'Get User Following List by u_id',
         params={'u_id': 'User ID'},
@@ -298,15 +323,24 @@ class following_list(Resource):
     )
     @api.marshal_list_with(user_model, code=200)
     def get(self, u_id):
-        users = db.session.query(User.following_list).filter(User.u_id == u_id).first()[0]
-        result = db.session.query(User).filter(User.u_id.in_(str_to_list(users))).all()
-        if result:
-            return result, 200
+        user = db.session.query(User).filter(User.u_id == u_id).first()
+        if user:
+            result = user.followed.all()
+            if result != []:
+                return result, 200
+            else:
+                return {
+                    'message': 'Empty list'
+                }, 401
         else:
             return {
                 'message': 'User not found'
             }, 401
 
+
+    ########################################
+    #      Add & Remove following list     #
+    ########################################
     @api.doc(
         description = 'Add User to Following List, remove if already in list',
         params = {
@@ -320,46 +354,40 @@ class following_list(Resource):
     )
     @api.expect(modify_list_argument, validate=True)
     def post(self, u_id):
-        follow_id = modify_list_argument.parse_args()['follow_id']
+        follow_id = modify_list_argument.parse_args()['target_id']
         user = db.session.query(User).filter(User.u_id == u_id).first()
-        following_list = str_to_list(user.following_list)
-        if user and db.session.query(exists().where(User.u_id == follow_id)).scalar():
-            if follow_id == user.u_id:
-                return {
-                    'message': 'Cannot follow yourself'
-                }, 403
+        follow_user = db.session.query(User).filter(User.u_id == follow_id).first()
 
-            if follow_id not in following_list:
-                # add user to following list
-                if user.following_list:
-                    user.following_list += follow_id
-                else:
-                    user.following_list = follow_id
-                db.session.query(User).filter(User.u_id == u_id).update({'following_list': user.following_list})
+        if u_id == follow_id:
+            return {
+                'message': 'Cannot follow yourself'
+            }, 403
+
+        if user and follow_user:
+            if follow_user in user.followed.all():
+                user.followed.remove(follow_user)
+                db.session.commit()
+                return {
+                    'message': 'User removed from following list'
+                }, 200
+            else:
+                user.followed.append(follow_user)
                 db.session.commit()
                 return {
                     'message': 'User added to following list'
                 }, 200
-            else:
-                # remove user from following list
-                following_list.remove(follow_id)
-
-                if len(following_list) == 0:
-                    db.session.query(User).filter(User.u_id == u_id).update({'following_list': None})
-                else:
-                    db.session.query(User).filter(User.u_id == u_id).update({'following_list': list_to_str(following_list)})
-                db.session.commit()
-                return {
-                    'message': 'User removed from following list'
-                }, 403
         else:
             return {
                 'message': 'User not found'
             }, 401
 
+
 @api.route('/auth/user/<string:u_id>/black_list', methods=['GET','POST'])
 class black_list(Resource):
 
+    ########################################
+    #            Get black list            #
+    ########################################
     @api.doc(
         description = "Get User's Black List",
         params={'u_id': 'User ID'},
@@ -370,15 +398,19 @@ class black_list(Resource):
     )
     @api.marshal_list_with(user_model, code=200)
     def get(self, u_id):
-        users = db.session.query(User.black_list).filter(User.u_id == u_id).first()[0]
-        result = db.session.query(User).filter(User.u_id.in_(str_to_list(users))).all()
-        if result:
-            return result, 200
-        else:
-            return {
-                'message': 'User not found'
-            }, 401
+        user = db.session.query(User).filter(User.u_id == u_id).first()
+        if user:
+            result = user.blocked.all()
+            if result != []:
+                return result, 200
+            else:
+                return {
+                    'message': 'Empty list'
+                }, 401
 
+    ########################################
+    #       Add & Remove black list        #
+    ########################################
     @api.doc(
         description = 'Add User to black List, remove if already in list',
         params = {
@@ -392,57 +424,29 @@ class black_list(Resource):
     )
     @api.expect(modify_list_argument, validate=True)
     def post(self, u_id):
-        black_id = modify_list_argument.parse_args()['u_id']
+        black_id = modify_list_argument.parse_args()['target_id']
         user = db.session.query(User).filter(User.u_id == u_id).first()
-        black_list = str_to_list(user.black_list)
-        if user and db.session.query(exists().where(User.u_id == black_id)).scalar():
-            if black_id == user.u_id:
+        black_user = db.session.query(User).filter(User.u_id == black_id).first()
+
+        if u_id == black_id:
+            return {
+                'message': 'Cannot black yourself'
+            }, 403
+        
+        if user and black_user:
+            if black_user in user.blocked.all():
+                user.blocked.remove(black_user)
+                db.session.commit()
                 return {
-                    'message': 'Cannot put yourself into black list'
-                }, 403
-                
-            if black_id not in black_list:
-                # add user to black list
-                if user.black_list:
-                    user.black_list += black_id
-                else:
-                    user.black_list = black_id
-                db.session.query(User).filter(User.u_id == u_id).update({'black_list': user.black_list})
+                    'message': 'User removed from black list'
+                }, 200
+            else:
+                user.blocked.append(black_user)
                 db.session.commit()
                 return {
                     'message': 'User added to black list'
                 }, 200
-            else:
-                # remove user from black list
-                black_list.remove(black_id)
-                if len(black_list) == 0:
-                    db.session.query(User).filter(User.u_id == u_id).update({'black_list': None})
-                else:
-                    db.session.query(User).filter(User.u_id == u_id).update({'black_list': list_to_str(black_list)})
-                db.session.commit()
-                return {
-                    'message': 'User removed from black list'
-                }, 403
         else:
             return {
                 'message': 'User not found'
             }, 401
-
-################################################################################
-#                                 HELPING FUNCS                                #
-################################################################################
-
-def list_to_str(following_list):
-    following_list_string = ''
-    for i in following_list:
-        following_list_string += i
-    if following_list_string == '':
-        following_list_string = None
-    return following_list_string
-
-
-def str_to_list(following_list_string):
-    if following_list_string:
-        return [following_list_string[i:i+32] for i in range(0, len(following_list_string), 32)]
-    else:
-        return []
