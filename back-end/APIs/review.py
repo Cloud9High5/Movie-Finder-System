5fe7561523cee1207e3e169b01b79df9960dcf02
Otto <zzy801997@163.com>
2022-08-01 19:52:24 +1000

    bug fix

--- back-end/APIs/review.py
+++ back-end/APIs/review.py
@@ -252,6 +252,11 @@ class reviews(Resource):
                 return {'message': 'review not found'}, 404
             else:
                 if review.u_id == current_user.u_id or current_user.is_admin:
+                    # detele related likes and dislikes
+                    db.session.query(Review_Like).filter_by(r_id=args['r_id']).delete()
+                    db.session.query(Review_Dislike).filter_by(r_id=args['r_id']).delete()
+                    db.session.commit()
+                    # delete review
                     db.session.delete(review)
                     db.session.commit()
                     return {'message': 'review deleted'}, 200
