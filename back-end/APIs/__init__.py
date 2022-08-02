7543f10ae504e9817d7d0886ca0ca7af4b16d8a8
Otto <zzy801997@163.com>
2022-08-02 13:20:51 +1000

    modify update user profile status code and remove unused import

--- back-end/APIs/__init__.py
+++ back-end/APIs/__init__.py
@@ -1,6 +1,5 @@
-from sys import prefix
 from flask import Blueprint
-from flask_restx import Api, Resource
+from flask_restx import Api
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
--- back-end/APIs/auth.py
+++ back-end/APIs/auth.py
@@ -1,12 +1,10 @@
-from email import message
 import json
-from re import U
-from flask import request, jsonify
+from flask import request
 from flask_restx import Resource, Namespace, fields, reqparse
 from flask_mail import Message
 from flask_jwt_extended import create_access_token, jwt_required, current_user
 from sqlalchemy import exists
-from extensions import db, mail, jwt
+from extensions import db, mail
 from Models.model import User, Film, Review
 
 api = Namespace("auth", description="Authentication related operations", path="/")
@@ -324,7 +322,7 @@ class user_info(Resource):
             if 'username' in payload:
                 # check if username is taken
                 if db.session.query(exists().where(User.username == payload['username'])).scalar():
-                    return {'message': 'Username taken'}, 403
+                    return {'message': 'Username taken'}, 405
                 else:
                     user.username = payload['username']
             if 'new_password' in payload:
